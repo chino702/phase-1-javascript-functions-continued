9bf464126388436c1b34fed01d32f544df643f0b
chino702 <chinox3702@gmail.com>
2023-03-22 10:17:35 -0700

    JS fucntions continued

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate"){
+let message = `This Saturday, I want to ${activity}!`;
+console.log(message);
+return message;
+}
+
+function mondayWork(activity = "go to the office"){
+    let message = `This Monday, I will ${activity}.`;
+    console.log(message);
+    return message;
+}
+
+function wrapAdjective(symbol) {
+    return function(adjective) {
+      return `You are ${symbol}${adjective}${symbol}!`;
+    }
+  }
+  
